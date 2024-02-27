c7b0d220b36ae73ddfe028ac6b464e260b325634
Andor Kesselman <andor@magic-pro-3.local>
2024-02-26 20:39:31 -0800

    added crypto and models

---
+++ src/models.ts
@@ -0,0 +1,22 @@
+export type ServiceProfileMetadata = {
+  id: string;
+  type?: string;
+  created?: string;
+  description?: string;
+  short_description?: string;
+  docs_url?: string;
+  supported_protocols?: string[];
+  tags?: string[];
+  version?: string;
+};
+
+export class ServiceProfile {
+  metadata: ServiceProfileMetadata;
+  constructor(metadata: ServiceProfileMetadata) {
+    this.metadata = metadata;
+  }
+  async getProfile() {
+    return "ServiceProfile";
+  }
+  async sign(signer: Signer) {}
+}
